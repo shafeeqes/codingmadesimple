1e6a637690b465ab1318b75b4961dfe8463e4386
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-09-26 22:57:20 +0530

    Update CheckIfBST.cpp

--- Binary Search Trees/CheckIfBST.cpp
+++ Binary Search Trees/CheckIfBST.cpp
@@ -50,3 +50,30 @@ bool isBST(Node* root){
     
     return false;    
 }
+
+//Alt solution
+/*
+int isBST(Node* node)  
+{  
+    return(isBSTUtil(node, INT_MIN, INT_MAX));  
+}  */
+  
+/* Returns true if the given tree is a BST and its values 
+are >= min and <= max. */
+int isBSTUtil(Node* node, int min, int max)  
+{  
+    /* an empty tree is BST */
+    if (node==NULL)  
+        return 1;  
+              
+    /* false if this node violates 
+    the min/max constraint */
+    if (node->data < min || node->data > max)  
+        return 0;  
+      
+    /* otherwise check the subtrees recursively,  
+    tightening the min or max constraint */
+    return
+        isBSTUtil(node->left, min, node->data-1) && // Allow only distinct values  
+        isBSTUtil(node->right, node->data+1, max); // Allow only distinct values  
+}  
