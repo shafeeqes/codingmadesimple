674c1ef7746d90f890f2274176d1ace2d7c156b4
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-09-27 15:47:53 +0530

    Update NodesAtSameLevel.cpp

--- Binary Search Trees/NodesAtSameLevel.cpp
+++ Binary Search Trees/NodesAtSameLevel.cpp
@@ -4,9 +4,9 @@
 Initially, all the nextRight pointers point to garbage values. Your function should set these pointers to point next right for each node.
        10                       10 ------> NULL
       / \                       /      \
-     3   5       =>     3 ------> 5 --------> NULL
+     3   5       =>           3 ------> 5 --------> NULL
     / \     \               /  \           \
-   4   1   2          4 --> 1 -----> 2 -------> NULL
+   4   1     2             4 --> 1 ----->   2 -------> NULL
 */
 
 /* struct Node
@@ -21,6 +21,39 @@ Initially, all the nextRight pointers point to garbage values. Your function sho
 // O(n) space
 //O(n) time
 
+
+void connect(Node *root)
+{
+    if(root == NULL)
+        return;
+    queue<Node*> p;
+    p.push(root);
+    root->nextRight = NULL;
+    
+    while(!p.empty()){
+        int size = p.size();
+        for(int i =0; i<size; i++){
+            Node *curr = p.front();
+            p.pop();
+            if(curr->left)
+                p.push(curr->left);
+            if(curr->right)
+                p.push(curr->right);
+        }
+        queue<Node*> c = p;
+        while(!c.empty()){
+            Node *curr = c.front();
+            c.pop();
+            if(!c.empty()){
+                curr->nextRight = c.front();
+            }
+            else
+                curr->nextRight = NULL;
+        }
+    }
+}
+
+//Alternate solution almost similar approach
 void connect(Node *root)
 {
     queue<Node*> parent, children;
@@ -32,7 +65,6 @@ void connect(Node *root)
     
     while(true){
         while(!parent.empty()){
-
             curr = parent.front();
             parent.pop();
             if(curr->left)
@@ -45,7 +77,6 @@ void connect(Node *root)
         parent = children;
         
         while(!children.empty()){
-            
             curr = children.front();
             children.pop();
             if(!children.empty())
@@ -55,7 +86,6 @@ void connect(Node *root)
         }
     }
 }
-
 
 
 //If in the question it is given it is a perfect BST, then 
@@ -93,3 +123,4 @@ void connect(Node *root)
         }
     }
 }
+
