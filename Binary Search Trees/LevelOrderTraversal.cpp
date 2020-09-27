3f93b28a30e64fb3da0929f4de695594d4ec0f2b
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-09-27 21:23:20 +0530

    Update LevelOrderTraversal.cpp

--- Binary Search Trees/LevelOrderTraversal.cpp
+++ Binary Search Trees/LevelOrderTraversal.cpp
@@ -1,11 +1,51 @@
+/*
+Given a binary tree, find its level order traversal.
+Level order traversal of a tree is breadth-first traversal for the tree.
 
-void connect(Node *root)
+Example 1:
+
+Input:
+    1
+  /   \ 
+ 3     2
+Output:1 3 2
+
+Example 2:
+
+Input:
+        10
+     /      \
+    20       30
+  /   \
+ 40   60
+Output:10 20 30 40 60 N N
+
+Your Task:
+You don't have to take any input. Complete the function levelOrder() that takes the root node as input parameter and returns a list of integers containing the level order traversal of the given Binary Tree.
+
+
+*/
+/*struct Node
 {
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+vector<int> levelOrder(Node* root)
+{
+    vector<int> ans;
     if(root == NULL)
-        return;
+        return ans;
     queue<Node*> p;
     p.push(root);
-    root->nextRight = NULL;
+    ans.push_back(root->data);
     
     while(!p.empty()){
         int size = p.size();
@@ -21,13 +61,10 @@ void connect(Node *root)
         }
         queue<Node*> c = p;
         while(!c.empty()){
-            Node *curr = c.front();
+            ans.push_back(c.front()->data);
             c.pop();
-            if(!c.empty()){
-                curr->nextRight = c.front();
-            }
-            else
-                curr->nextRight = NULL;
         }
     }
+    return ans;
+
 }
