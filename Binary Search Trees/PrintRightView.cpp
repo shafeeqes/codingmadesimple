949816a20f9edd62610c087cf00a78ea5626e71e
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-09-27 15:32:52 +0530

    Create PrintRightView.cpp

---
+++ Binary Search Trees/PrintRightView.cpp
@@ -0,0 +1,51 @@
+  
+/*Given a Binary Tree, print Left view of it. Left view of a Binary Tree is
+  set of nodes visible when tree is visited from Left side. 
+  The task is to complete the function rightView(), which accepts
+  root of the tree as argument.
+Left view of following tree is 1 3 7 8.
+           1
+        /    \
+     2         3
+   /   \     /    \
+  4     5   6      7
+   \
+     8   
+  */
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+//Time Complexiy: O(n)
+//Space complexity: O(n)
+vector<int> rightView(Node *root)
+{
+    vector<int> ans;
+    if(root == NULL)
+        return ans;
+    queue<Node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int size = q.size();
+        for(int i =0; i<size; i++){
+            Node *curr = q.front();
+            q.pop();
+            if(i==0)
+                ans.push_back((curr->data));
+            if(curr->left)
+                q.push(curr->right);
+            if(curr->right)
+                q.push(curr->left);
+        }
+    }
+    return ans;
+}
