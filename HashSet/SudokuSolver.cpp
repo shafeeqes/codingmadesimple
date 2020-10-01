1e8474613f8556a958533907182ce1585e8a9d0a
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-01 16:07:11 +0530

    Update SudokuSolver.cpp

--- HashSet/SudokuSolver.cpp
+++ HashSet/SudokuSolver.cpp
@@ -26,12 +26,12 @@ public:
     void solveSudoku(vector<vector<char>>& board) {
         sign = false;
         solve(board,0);
-        
     }
-    
-      
+        
     void solve(vector<vector<char>>&board,int n){
         if(n>80){
+            //if n = 81 only one entry will be valid. so 
+            //the board is solved
             sign = true;
             return;
         }
@@ -40,31 +40,29 @@ public:
         }
         else{
             for(int i=1; i<=9; i++){
-                
+                //check for valid entry
                 if(check(board,n/9,n%9,i)){
                     board[n/9][n%9]= i +'0';
                     solve(board,n+1);
                     if(sign == true)
                         return;
+                    //If the current value does not return a solved 
+                    //puzzle set it back to '.' and try the next number
                     board[n/9][n%9]='.';
                 }
             }
         }
-        
     }
     
-    bool check(vector<vector<char>>& board,int row,int col,int val)
-    {
-        
+    bool check(vector<vector<char>>& board,int row,int col,int val){
+        //no same value in the same row or same column
         for(int i=0;i<9;i++){
             if(board[row][i]== val+'0' or board[i][col] == val+'0') 
                 return false;
         }
-        
         int x=3*(row/3);
         int y=3*(col/3);
-        
-        
+        //checking for value in same box    
         for(int i=x;i<x+3;i++){
             for(int j=y;j<y+3;j++){
                 if(board[i][j]== val+'0') return false;
