03c8a4077416def1b3e402d1704b21a5f29f833d
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-03 18:53:13 +0530

    Update JumpGameII.cpp

--- Arrays/JumpGameII.cpp
+++ Arrays/JumpGameII.cpp
@@ -16,16 +16,24 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         
-        int pos = 0, des = 0;
-        int count = 0;
-        
-        for(int i = 0;i< nums.size()-1;i++){
-            des = max(des, i+nums[i]);
-            if(pos == i){
-                pos = des;
-                count++;
-            } 
+        if (nums.size() == 1) {
+            return 0;
         }
-        return count;
+        int count = 0;
+        int i = 0;
+        while (i + nums[i] < nums.size() - 1) {
+            int maxVal = 0;
+            int maxValIndex = 0;
+            for (int j = 1; j <= nums[i]; j++) {
+                if (nums[j + i] + j > maxVal) {
+                    maxVal = nums[j + i] + j;
+                    maxValIndex = i + j;
+                }
+            }
+            i = maxValIndex;
+            count++;
+        }
+        return count + 1;
+        
     }
 };
