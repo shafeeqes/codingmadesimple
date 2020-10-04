36efc4f326fc129837b47e25bd3a4514415c98ff
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-04 22:03:45 +0530

    Update SpecialKeyboard.cpp

--- Dynamic Programming/SpecialKeyboard.cpp
+++ Dynamic Programming/SpecialKeyboard.cpp
@@ -120,3 +120,36 @@ public:
         return screen[N - 1]; 
     }
 };
+
+
+
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int N;
+		cin >> N;
+		int dp[N+5];
+		for(int i=1;i<=min(N,6);i++)
+			dp[i] = i;
+
+		for(int i=7;i<=N;i++)
+		{
+			int ans = 0;
+			for(int j=i-3;j>=1;j--)
+			{
+				int curr = (i-j-1)*dp[j];
+				ans = max(ans,curr);
+			}
+			dp[i] = ans;
+		}
+		if (N > 75)
+			cout << -1 << endl;
+		else
+			cout << dp[N] << endl;
+	}
+}
