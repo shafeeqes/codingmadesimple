7accf6d93800c5b4eccb4e60773c02971690b2c9
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-06 01:08:25 +0530

    Create SortCharactersbyFrequency.cpp

---
+++ HashSet/SortCharactersbyFrequency.cpp
@@ -0,0 +1,69 @@
+/*
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+*/
+
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> hash;
+        for(char a:s){
+            hash[a]++;
+        }
+        
+        vector<pair<char, int> > A;
+        for(auto it:hash){
+            A.push_back(it);
+        }
+        sort(A.begin(),A.end(),cmp);
+        
+        string ans = "";
+        for(auto item:A){
+            while(item.second--){
+                ans+= item.first;
+            }
+        }
+        return ans;
+    }
+    
+    static bool cmp(pair<char, int> & a, pair<char, int> & b) 
+    { 
+        return a.second > b.second; 
+    } 
+};
+
+
