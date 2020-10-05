5c83d2681277dbba28826455bea454ef8f2b3c88
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-06 01:22:10 +0530

    Update SortCharactersbyFrequency.cpp

--- HashSet/SortCharactersbyFrequency.cpp
+++ HashSet/SortCharactersbyFrequency.cpp
@@ -99,3 +99,26 @@ public:
     }
 };
 
+
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freqMap; // {char, frequency}
+        multimap<int, char, greater<int>> sortedFreqsMap;
+        string res = "";
+        for (char chr : s) {
+            freqMap[chr]++;
+        }
+        for (auto tuple : freqMap) {
+            sortedFreqsMap.insert({tuple.second, tuple.first});
+        }
+        for (auto tuple : sortedFreqsMap) {
+            int freq = tuple.first;
+            while (freq--) {
+                res += tuple.second;
+            }
+        }
+        return res;
+    }
+};
