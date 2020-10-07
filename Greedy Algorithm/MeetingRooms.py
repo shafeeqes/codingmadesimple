79fe9ee7f665e7a44fa8bb3ab0ddef9f90d94437
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-07 11:57:32 +0530

    Update MeetingRooms.py

--- Greedy Algorithm/MeetingRooms.py
+++ Greedy Algorithm/MeetingRooms.py
@@ -106,3 +106,27 @@ Explanation 2:
             maxrooms = max(maxrooms,curr_rooms)
             
         return maxrooms
+       
+       
+       
+  """
+  
+int solve2(vector<vector<int> > &A){
+    int n=A.size();
+    sort(A.begin(),A.end());
+        
+        priority_queue<int, vector<int>, greater<int>> heap;
+        int rooms = 0;
+        heap.push(A[0][1]);
+        rooms++;
+        for (int i = 1; i < n; i++) {
+            if (A[i][0] < heap.top()) {
+                rooms++;
+            } else {
+                heap.pop();
+            }
+            heap.push(A[i][1]);
+        }
+        return rooms;
+}
+  """
