58ab326b67761a9863d7ce39242b6ef928fa374f
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-07 10:06:03 +0530

    Create Stairs.java

---
+++ Dynamic Programming/Stairs.java
@@ -0,0 +1,71 @@
+/*
+ou are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+*/
+
+
+// O(n) time O(n) space
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+
+//O(n) time O(1) space
+public class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int c;
+        for(int i = 0; i < n; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+
+//O(logn)   O(1) space
+
+public class Solution {
+    public int climbStairs(int n) {
+        double sqrt5=Math.sqrt(5);
+        double fibn=Math.pow((1+sqrt5)/2,n+1)-Math.pow((1-sqrt5)/2,n+1);
+        return (int)(fibn/sqrt5);
+    }
+}
