60b48ad505463194abbf87a55f9efbb3cd205d77
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-10-09 20:19:43 +0530

    Update BellmanFord.cpp

--- Graphs/BellmanFord.cpp
+++ Graphs/BellmanFord.cpp
@@ -1,3 +1,10 @@
+/*
+The shortest path problem is about finding a path between  vertices in a graph such that the total sum of the edges weights is minimum.
+
+This problem could be solved easily using (BFS) if all edge weights were (), but here weights can take any value. 
+Three different algorithms are discussed below depending on the use-case.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
