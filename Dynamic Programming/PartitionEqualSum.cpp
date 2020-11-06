eaadcc330bfc0fa16103debd4c12900da7ee0e74
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-11-07 00:28:52 +0530

    Update PartitionEqualSum.cpp

--- Dynamic Programming/PartitionEqualSum.cpp
+++ Dynamic Programming/PartitionEqualSum.cpp
@@ -28,37 +28,39 @@ Explanation: The array cannot be partitioned into equal sum subsets.
 
 class Solution {
 public:
-    unordered_map<int,unordered_map<int,bool>> dp;
     bool canPartition(vector<int>& A) {
      
         int total = 0;
         for(int i:A){
             total += i;
         }
-        
         if(total %2 != 0)
             return false;
         
-        return solve(A,0,total/2);
+        int n = A.size();
+        int subsetSum = total/2;
+        vector<vector<optional<bool>>> dp(n + 1, vector<optional<bool>> (subsetSum +1, nullopt));
+   
+        return dfs1(A, 0, subsetSum, dp);
         
     }
-    bool solve(vector<int> &A, int index, int sum){
+    bool dfs(vector<int> &A, int index, int sum, vector<vector<optional<bool>>> &dp){
         
-        if(dp[index].find(sum)!= dp[index].end()){
-            return dp[index][sum];
-        }
         if(sum == 0)
             return true;
         
-        if(sum < 0 or index == A.size())
+        if(sum < 0 or index >= A.size())
             return false;
         
-        for(int i = index; i < A.size(); i++){
-            dp[i][sum] = solve(A, i+1, sum-A[i]);
-            
-            if(dp[i][sum])
-               return 1;
+        if(dp[index][sum] != nullopt){
+            return (dp[index][sum] == true);
         }
-        return dp[index][sum] = 0;
+    
+        //either include this number or not include this number
+        bool result = dfs(A, index + 1, sum, dp) or dfs(A, index+1, sum - A[index], dp);
+        
+        dp[index][sum] = result;
+        
+        return result;
     }
 };
