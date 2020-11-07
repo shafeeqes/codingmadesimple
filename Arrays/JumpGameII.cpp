3a6904e75d92ad9c213958aae52b6112e718877e
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-11-07 17:12:51 +0530

    Update JumpGameII.cpp

--- Arrays/JumpGameII.cpp
+++ Arrays/JumpGameII.cpp
@@ -12,6 +12,34 @@ Explanation: The minimum number of jumps to reach the last index is 2.
     Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
 */
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        if (nums.size() == 1) {
+            return 0;
+        }
+        int count = 0;
+        int i = 0;
+        while (i + nums[i] < nums.size() - 1) {
+            int maxVal = 0;
+            int maxValIndex = 0;
+            for (int j = i + 1; j <= i + nums[i]; j++) {
+                if (nums[j] + j  > maxVal) {
+                    maxVal = nums[j ] + j ;
+                    maxValIndex = j;
+                }
+            }
+            i = maxValIndex;
+            count++;
+        }
+        return count + 1;
+        
+    }
+};
+
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
