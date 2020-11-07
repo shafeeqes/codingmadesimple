a1c4190e83ed39f0993582f14de323dad1f2b187
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-11-08 00:36:04 +0530

    Update DetectCycleinaDirectedGraph.cpp

--- Graphs/DetectCycleinaDirectedGraph.cpp
+++ Graphs/DetectCycleinaDirectedGraph.cpp
@@ -44,6 +44,83 @@ From graph it is clear that it contains cycle.
 */
 
 
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+/*  Function to check if the given graph contains cycle
+*   V: number of vertices
+*   adj[]: representation of graph
+*/
+bool helper(vector<int> graph[], vector<int> &visited, int node)
+{
+	// 0- not processed, 1 - processing , 2- processed
+	//here unlike undirected graph, where when we encounter a processed node and we return true
+	//here we return true when a currently processing node is enocuntered
+    if(visited[node] == 1)
+        return true;
+    
+    if(visited[node] == 2)
+        return false;
+        
+    visited[node] = 1;
+    for(int i = 0; i< graph[node].size();++i)
+    {   int curr = graph[node][i];
+        if(helper(graph, visited, curr))
+            return true;
+    }
+    visited[node] = 2;
+    return false;
+}
+
+bool isCyclic(int V, vector<int> graph[]){
+    
+    vector<int> visited(V,0);
+    
+    for(int i = 0;i < V; ++i){
+        if(!visited[i]){
+            if(helper(graph, visited, i))
+                return true;
+        }            
+    }
+    return false;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin >> t;
+	
+	while(t--){
+	    
+	    int v, e;
+	    cin >> v >> e;
+	    
+	    vector<int> adj[v];
+	    
+	    for(int i =0;i<e;i++){
+	        int u, v;
+	        cin >> u >> v;
+	        adj[u].push_back(v);
+	    }
+	    
+	    cout << isCyclic(v, adj) << endl;
+	    
+	}
+	
+	return 0;
+}  // } Driver Code Ends
+
+
+
+//similar solution
+
 
 // { Driver Code Starts
 #include <bits/stdc++.h>
