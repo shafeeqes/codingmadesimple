0bc489b41417b82d3637c594ebbacb5f5c1442dc
shafeeqes <32516833+shafeeqes@users.noreply.github.com>
2020-11-07 10:33:40 +0530

    Update TargetSum.cpp

--- Dynamic Programming/TargetSum.cpp
+++ Dynamic Programming/TargetSum.cpp
@@ -48,7 +48,56 @@ Find a subset P of nums such that sum(P) = (target + sum(nums)) / 2
 Note that the above formula has proved that target + sum(nums) must be even
 */
 
+//O(n*s) space
+// O(n*s) time
 
+//NB: while counting ways unlike the other similar problems, j should start from 0. not 1
+
+class Solution {
+public:
+        
+        //we can consider the whole array into two sets, one with positive values 
+        //and one with negative
+        //then S1 + S2 = SUM
+        //S1- S2 = target
+        //S1 - (SUM-S1) = target
+        // 2S1 - SUM = target
+        // S1 = (SUM + target)/2
+        
+       
+    int findTargetSumWays(vector<int>& nums, int s) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        return sum < s || (s + sum) & 1 ? 0 : subsetSum(nums, (s + sum) >> 1); 
+    }   
+
+    int subsetSum(vector<int>& nums, int sum) {
+       
+        int n = nums.size();
+        int dp[n+1][sum+1];
+        memset(dp, 0, sizeof(dp));
+        
+        for(int i = 0; i <=n ; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= sum; j++){
+                dp[i][j] = dp[i-1][j];
+                
+                if(nums[i-1] <= j){
+                    dp[i][j] += dp[i-1][j - nums[i-1]];
+                }
+            }
+        }
+      
+        return dp[n][sum];
+     
+    }
+    
+};
+
+//O(s) space
+// O(n*s) time
 
 class Solution {
 public:
